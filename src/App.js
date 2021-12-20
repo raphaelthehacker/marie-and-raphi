a9ca4cce70134d533c915c4aaee738d6b7fbd50b
Raphael Wirth <raphael.wirth@helvetia.ch>
2021-12-20 15:54:59 +0100

    ptimized images and add stuff for pwa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import marieundraphi from './assets/marieundraphi.jpg';
 import shesaidyes from './assets/shesaidyes.gif';
-import Confetti from 'react-confetti'
 import './App.css';
-import { useState } from 'react';
+
+import React, { Suspense, useState } from 'react';
+
+const Confetti = React.lazy(() => import('react-confetti'));
 
 function App() {
 
@@ -22,12 +24,15 @@ function App() {
   return (
 
     <div onTouchStart={(startConfettiParty)} onMouseDown={startConfettiParty} onTouchEnd={stopConfettiParty} onMouseUp={stopConfettiParty} className="app">
-      <Confetti
-        width={window.innerWidth}
-        height={window.innerHeight}
-        gravity={gravity}
-        numberOfPieces={numberOfPieces}
-      />
+      <Suspense fallback={<div></div>}>
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          gravity={gravity}
+          numberOfPieces={numberOfPieces}
+        />
+      </Suspense>
+
       <img src={shesaidyes} className="she-said-yes" alt="she-said-yes" />
       <img src={marieundraphi} className="marie-and-raphi" alt="marie und raphi" />
     </div>
